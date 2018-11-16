63070a08eff5c3bfcff411901263adf335a58e5a
BackBeing <30850397+BackBeing@users.noreply.github.com>
2018-11-16 11:10:45 -0600

    Update s.sh

--- s.sh
+++ s.sh
@@ -5,7 +5,7 @@ do
 	do
 		for rob in 4 8 16 32 64 128 256
 		do
-		$GEM5/build/ARM/gem5.opt hw3config.py -c $GEM5/../test_progs/daxpy/daxpy_arm_big --cpu-type="DerivO3CPU" --caches --l2cache --num-phys-float-regs=$pf --num-rob-entries=$rob --num-iq-entries=$iq
+		$GEM5/build/ARM/gem5.opt hw3config.py -c $GEM5/../test_progs/daxpy/daxpy_arm_big --cpu-type="DerivO3CPU" --caches --l2cache --pf_num=$pf --rob_num=$rob --iq_num=$iq
 		grep -E 'sim_seconds|system.cpu.rename.fp_rename_lookups|system.cpu.iq.rate|system.cpu.rename.IdleCycles|system.cpu.rename.ROBFullEvents|system.l2.overall_mshr_miss_rate::total' m5out/stats.txt
 		done
 	done
